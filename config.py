42d33ecb9e3e7a037b0dcbf56226b539f292fe86
LiathDevelopment <71844824+LiathDevelopment@users.noreply.github.com>
2021-03-24 19:39:58 +0000

    Add .gitignore

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 main_config = {
     "settings": {
         "prefix": "~",
-        "devprefix": ">>",
-        "token": "TOKEN",
-        "devtoken": "TOKEN",
-        "modlog-channel": 000000000000000000, #needs to be an int
-        "devmodlog-channel": 00000000000000000, #needs to be an int
+        "debugPrefix": ">>",
+        "token": "Token",
+        "debugToken": "Token",
+        "modlog-channel": 000, #needs to be an int
+        "debugModlog-channel": 000, #needs to be an int
         "maxchars": 256, #needs to be an int
-        "dev-id": 000000000000000000 #needs to be an int
+        "dev-id": 567406113809498113, #needs to be an int
+        "debugMode": True #needs to be a boolean
     }
 }
 
