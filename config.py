42ffc1a3ede03bbd7d7f875b940374f8725f1a17
LiathDevelopment <71844824+LiathDevelopment@users.noreply.github.com>
2021-03-24 19:30:15 +0000

    Upload config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+main_config = {
+    "settings": {
+        "prefix": "~",
+        "debugPrefix": ">>",
+        "token": "TOKEN",
+        "debugToken": "TOKEN",
+        "modlog-channel": 0,  # needs to be an int
+        "debugModlog-channel": 0,  # needs to be an int
+        "maxchars": 256,  # needs to be an int
+        "dev-id": 567406113809498113,  # needs to be an int
+        "debugMode": True  # needs to be a boolean
+    }
+}
+
+config = main_config["settings"]
